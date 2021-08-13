932ab7638608dc445dcee3ca892041c3ed2900af
Amirhossein Moghadam <Amirhoussein.Moghadam79@gmail.com>
2021-08-13 20:35:45 +0430

    Test

--- color-button/src/App.jsx
+++ color-button/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 
+export function replaceCamelWithSpace(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, " $1");
+}
+
 function App() {
   const [colourButton, setColourButton] = useState("red");
   const [disabled, setDisabled] = useState(false);
@@ -18,17 +22,22 @@ function App() {
     <div>
       <button
         onClick={handleOnClick}
-        style={{ backgroundColor: colourButton, color: "white" }}
+        style={{
+          backgroundColor: disabled ? "gray" : colourButton,
+          color: "white",
+        }}
         disabled={disabled}
       >
         Change To {newButtonClick}
       </button>
       <input
         type="checkbox"
+        id="disbale-button-checkbox"
         onChange={handleOnChage}
         defaultChecked={disabled}
         aria-checked={disabled}
       />
+      <label htmlFor="disbale-button-checkbox">Disabled Button</label>
     </div>
   );
 }
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,6 +1,7 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import App from "./App.jsx";
+import { replaceCamelWithSpace } from "./App";
 
 test("button has  correct initial color", () => {
   render(<App />);
@@ -37,15 +38,62 @@ test("Checkbox disables button on first click and enable on second click", () =>
   render(<App />);
 
   const buttonColor = screen.getByRole("button");
-  const checkBox = screen.getByRole("checkbox");
+  const checkBox = screen.getByRole("checkbox", { name: /disabled button/i });
 
   expect(buttonColor).toBeEnabled();
 
   fireEvent.click(checkBox);
   expect(checkBox).toBeChecked();
   expect(buttonColor).toBeDisabled();
-// sample
+
   fireEvent.click(checkBox);
   expect(checkBox).not.toBeChecked();
   expect(buttonColor).toBeEnabled();
+});
+
+test("Disable button has gray background and reverts to red", () => {
+  render(<App />);
+  const checkBox = screen.getByRole("checkbox", { name: /disabled button/i });
+  const colorButton = screen.getByRole("button", { name: "Change To blue" });
+
+  // disable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
+
+  // re-enable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("Clicked disabled button has gray background and reverts to blue", () => {
+  render(<App />);
+  const checkBox = screen.getByRole("checkbox", { name: /disabled button/i });
+  const colorButton = screen.getByRole("button", { name: "Change To blue" });
+
+  // change button to blue
+  fireEvent.click(colorButton);
+
+  // disable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: "gray" });
+
+  // re-enable button
+  fireEvent.click(checkBox);
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+});
+
+describe("Spaces before camel-case capital letter", () => {
+  test("works for no inner capital letter", () => {
+    expect(replaceCamelWithSpace("Red")).toBe("Red");
+  });
+
+  test("works for one inner capital letter", () => {
+    expect(replaceCamelWithSpace("MidnightBlue")).toBe("Midnight Blue");
+  });
+
+  test("works for multiple inner capital letter", () => {
+    expect(replaceCamelWithSpace("MediumViolentRed")).toBe(
+      "Medium Violent Red"
+    );
+  });
 });
